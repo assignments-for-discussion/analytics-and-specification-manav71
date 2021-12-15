1fefc505075bffca0194285d1c5dc64b3d4ef74f
Manav Sanghi <73513315+manav71@users.noreply.github.com>
2021-12-16 01:24:19 +0530

    Update average.js
    updated code for bypassing outliers.

--- average.js
+++ average.js
@@ -1,12 +1,39 @@
 
 function average(numbers) {
   
-const validData = numbers.filter(function (data) {
+//   filtering out NaN data points
   
-      return !Number.isNaN(data);
-  });
+  const dataSet = numbers.filter((data) => !Number.isNaN(data));
   
- return validData.reduce((p, c) => p + c, 0) / validData.length;
+  
+  dataSet.sort((a, b) => a - b);
+  
+  
+//   finding interquartile range (IQR) score
+  
+  var firstQuartile = dataSet[Math.floor(dataSet.length / 4)];
+  
+  var thirdQuartile = dataSet[Math.ceil(dataSet.length * (3 / 4)) - 1];
+  
+  var interquartileRange = thirdQuartile - firstQuartile;
+  
+  
+//   Setting upper and lower limits
+  
+  var upperLimit = thirdQuartile + interquartileRange * 1.5;
+  
+  var lowerLimit = firstQuartile - interquartileRange * 1.5;
+  
+  
+//   removing outliers
+  
+  var validDataSet = dataSet.filter((a) => a <= upperLimit && a >= lowerLimit );
+  
+  
+//   finding average
+  
+  return validDataSet.reduce((p, c) => p + c, 0) / validDataSet.length;
+  
 }
 
 module.exports = { average };
