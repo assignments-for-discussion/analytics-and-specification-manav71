9e665f702a03b959153a2c97ae8553b421bc2c12
Manav Sanghi <73513315+manav71@users.noreply.github.com>
2021-12-16 01:28:31 +0530

    Update average.js

--- average.js
+++ average.js
@@ -2,15 +2,12 @@
 function average(numbers) {
   
 //   filtering out NaN data points
-  
   const dataSet = numbers.filter((data) => !Number.isNaN(data));
-  
   
   dataSet.sort((a, b) => a - b);
   
   
 //   finding interquartile range (IQR) score
-  
   var firstQuartile = dataSet[Math.floor(dataSet.length / 4)];
   
   var thirdQuartile = dataSet[Math.ceil(dataSet.length * (3 / 4)) - 1];
@@ -19,19 +16,16 @@ function average(numbers) {
   
   
 //   Setting upper and lower limits
-  
   var upperLimit = thirdQuartile + interquartileRange * 1.5;
   
   var lowerLimit = firstQuartile - interquartileRange * 1.5;
   
   
 //   removing outliers
-  
   var validDataSet = dataSet.filter((a) => a <= upperLimit && a >= lowerLimit );
   
   
 //   finding average
-  
   return validDataSet.reduce((p, c) => p + c, 0) / validDataSet.length;
   
 }
