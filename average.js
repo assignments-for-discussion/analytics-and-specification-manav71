3459336ee4d7d58fe9d9bbbea255709bb63c48c3
Manav Sanghi <73513315+manav71@users.noreply.github.com>
2021-12-16 01:47:22 +0530

    Update average.js

--- average.js
+++ average.js
@@ -2,32 +2,27 @@
 function average(numbers) {
   
 //   filtering out NaN data points
-  const dataSet = numbers.filter((data) => !Number.isNaN(data));
-  
-  dataSet.sort((a, b) => a - b);
-  
-  
+const dataSet = numbers.filter((data) => !Number.isNaN(data));
+
+dataSet.sort((a, b) => a - b);
+   
 //   finding interquartile range (IQR) score
-  var firstQuartile = dataSet[Math.floor(dataSet.length / 4)];
+var firstQuartile = dataSet[Math.floor(dataSet.length / 4)];
   
-  var thirdQuartile = dataSet[Math.ceil(dataSet.length * (3 / 4)) - 1];
+var thirdQuartile = dataSet[Math.ceil(dataSet.length * (3 / 4)) - 1];
   
-  var interquartileRange = thirdQuartile - firstQuartile;
-  
+var interquartileRange = thirdQuartile - firstQuartile;
   
 //   Setting upper and lower limits
-  var upperLimit = thirdQuartile + interquartileRange * 1.5;
+var upperLimit = thirdQuartile + interquartileRange * 1.5;
   
-  var lowerLimit = firstQuartile - interquartileRange * 1.5;
-  
+var lowerLimit = firstQuartile - interquartileRange * 1.5;
   
 //   removing outliers
-  var validDataSet = dataSet.filter((a) => a <= upperLimit && a >= lowerLimit );
-  
+var validDataSet = dataSet.filter((a) => a <= upperLimit && a >= lowerLimit);
   
 //   finding average
-  return validDataSet.reduce((p, c) => p + c, 0) / validDataSet.length;
-  
+return validDataSet.reduce((p, c) => p + c, 0) / validDataSet.length; 
 }
 
 module.exports = { average };
