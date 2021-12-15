3742d619c31dda89e9f1d834e9f2bdbfee4a6cdf
Manav Sanghi <73513315+manav71@users.noreply.github.com>
2021-12-15 23:05:17 +0530

    Update average.test.js
    Adding test to supply outliers.

--- test/average.test.js
+++ test/average.test.js
@@ -14,3 +14,7 @@ it('reports the average as NaN on an empty list', ()=> {
 it('ignores NaN in the input', ()=> {
   expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
 });
+
+it('ignores outliers in the input', () => {
+  expect(average([3, 7, 1500, 20, 800, 10, 5])).to.be.approximately(9, 0.01);
+});
