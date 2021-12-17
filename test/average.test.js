84b1b554dc513dc7cd33149eb54a0f88fa257679
Manav Sanghi <73513315+manav71@users.noreply.github.com>
2021-12-17 18:27:02 +0530

    test for list containing only NaN values
    this test if for the list of input having only NaN values.

--- test/average.test.js
+++ test/average.test.js
@@ -23,3 +23,6 @@ it('ignores outliers and NaN in the input', () => {
   expect(average([5,NaN, 8, 11, 20, 2000])).to.be.approximately(11, 0.01);
 });
 
+it('reports the average as NaN on an list of NaN', ()=> {
+  expect(average([NaN, NaN, NaN, NaN])).to.be.NaN;
+});
